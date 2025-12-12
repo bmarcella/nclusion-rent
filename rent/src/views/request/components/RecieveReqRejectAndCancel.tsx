0a10c27829967d1ed10974cbc39539ea10cd5462
bmarcella <79896790+bmarcella@users.noreply.github.com>
2025-12-12 14:30:00 -0500

    Solve issue about My request

--- rent/src/views/request/components/RecieveReqRejectAndCancel.tsx
+++ rent/src/views/request/components/RecieveReqRejectAndCancel.tsx
@@ -1,6 +1,6 @@
 import ShowReq from './ShowReq'
 export default function RecieveReqRejectAndCancel() {
   return (
-    <ShowReq  forMe={true} transition={true} rejected={true} ></ShowReq> 
+    <ShowReq  transition={true} rejected={true} ></ShowReq> 
   )
 }
