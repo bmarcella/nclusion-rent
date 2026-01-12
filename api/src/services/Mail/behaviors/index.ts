bd383752e0c3309b19decf8729186e9573b6775f
bmarcella <79896790+bmarcella@users.noreply.github.com>
2026-01-12 13:05:53 -0500

    full clean up

--- api/src/services/Mail/behaviors/index.ts
+++ api/src/services/Mail/behaviors/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import { createService, DEvent } from "src/damba.import";
+import { createService, DEvent } from "../../../damba.import";
 
 
 const api = createService('/mail');
